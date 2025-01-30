b00d93ab5a2860fded5741f97475cbc095703fd5
Natalia Tarakci <nataliatarakci@gmail.com>
2025-01-30 22:22:41 +0000

    v2

---
+++ src/test/java/myapp/tests/US_02_Natalia_T.java
@@ -0,0 +1,10 @@
+package myapp.tests;
+
+import myapp.pages.HomePage;
+import myapp.pages.RegistrationPage;
+
+public class US_02_Natalia_T {
+
+    HomePage homePage = new HomePage();
+    RegistrationPage registrationPage = new RegistrationPage();
+}
