73c6c10a247e5444454d9bb73812da4e823b9800
Goychak M <musayevagoychak2@gmail.com>
2025-02-06 15:56:40 +0400

    v2_Goychak

--- src/test/java/myapp/tests/US_13_Goychak.java
+++ src/test/java/myapp/tests/US_13_Goychak.java
@@ -12,6 +12,35 @@ import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.assertEquals;
 
+/*
+TC_01
+1- Navigate to "https://allovercommerce.com/"
+2- Click on the Sign In tab located in the top-right corner of the website.
+3- Enter valid vendor credentials (Email & Password) in the login fields.
+4- Click on the Sign-In button to log into the account.
+5- Scroll to the footer of the page and click on My Account under the My Account section.
+6- On the My Account page, click on the Addresses tab.
+7- Click on the Edit your shipping address under the Shipping Address section.
+8- Enter valid details in all required fields: (first name, last name, country, city...)
+9- Click the Save Address button.
+10- Assert that the confirmation message "Address changed successfully." appears.
+
+
+
+TC_02
+1- Navigate to "https://allovercommerce.com/"
+2- Click on the Sign In tab located in the top-right corner of the website.
+3- Enter valid vendor credentials (Email & Password) in the login fields.
+4- Click on the Sign-In button to log into the account.
+5- Scroll to the footer of the page and click on My Account under the My Account section.
+6- On the My Account page, click on the Addresses tab.
+7- Click on the Edit your shipping address under the Shipping Address section.
+8- Enter incomplete details in the required fields (e.g., leave out the Street Address, City, or ZIP Code).
+9- Click the Save Address button.
+10- Assert that an appropriate error message is displayed (e.g., "ZIP Code is a required field.").
+
+*/
+
 public class US_13_Goychak {
     HomePage homePage;
     Login_Page loginPage;
@@ -22,7 +51,12 @@ public class US_13_Goychak {
     @BeforeMethod
     public void setUp() {
 
+        ExtentReportUtils.createTestReport("AllOverCommerce Project Test Report for US_13", "Testing the functionality of updating the shipping address.");
+
         Driver.getDriver().get(ConfigReader.getProperty("allOverCommerce_url"));
+
+        ExtentReportUtils.pass("User navigated to the page.");
+
         homePage = new HomePage();
         loginPage = new Login_Page();
         myAccountPage = new MyAccount_Page();
@@ -30,18 +64,28 @@ public class US_13_Goychak {
         faker = new Faker();
 
         homePage.signInOption.click();
+
+        ExtentReportUtils.pass("User clicked on Sign-In button.");
+
         loginPage.usernameEmailInput.sendKeys(ConfigReader.getProperty("allOverCommerce_vendorEmail"));
         loginPage.passwordInput.sendKeys(ConfigReader.getProperty("Password"));
         loginPage.signInButton.click();
+
+        ExtentReportUtils.pass("User entered valid credentials as a vendor.");
 
         ActionsUtils.actionsScrollDown();
         WaitUtils.waitFor(3);
         JSUtils.JSclickWithTimeout(homePage.myAccountTab);
         myAccountPage.addressesMenuTab.click();
+
+        ExtentReportUtils.pass("User scrolled down to the footer of the page and clicked on My Account tab.");
+
         WaitUtils.waitFor(3);
         ActionsUtils.actionsScrollDown();
         shippingAddressPage.editShippingAddress.click();
         WaitUtils.waitFor(5);
+
+        ExtentReportUtils.pass("User clicked on 'Edit Your Shipping Address' tab.");
 
     }
 
@@ -50,16 +94,29 @@ public class US_13_Goychak {
 
         shippingAddressPage.firstNameShipping.clear();
         shippingAddressPage.firstNameShipping.sendKeys(faker.name().firstName());
+
+        ExtentReportUtils.pass("User entered a valid name in the 'First Name' field.");
+
         shippingAddressPage.lastNameShipping.clear();
         shippingAddressPage.lastNameShipping.sendKeys(faker.name().lastName());
 
+        ExtentReportUtils.pass("User entered a valid last name in the 'Last Name' field.");
+
         BrowserUtils.dropdownSelectByVisibleText(shippingAddressPage.countryDropDownShipping, "Antarctica");
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User selected 'Antarctica' from the Country dropdown.");
+
         shippingAddressPage.streetAddressShipping.clear();
         shippingAddressPage.streetAddressShipping.sendKeys(faker.address().fullAddress());
+
+        ExtentReportUtils.pass("User entered a valid address in the 'Street address' field.");
+
         shippingAddressPage.cityShipping.clear();
         shippingAddressPage.cityShipping.sendKeys(faker.address().city());
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User entered a valid city in the 'City' field.");
 
         // State Field
             WaitUtils.waitForVisibility(shippingAddressPage.stateShipping, 1);
@@ -71,6 +128,7 @@ public class US_13_Goychak {
                 System.out.println("State field not visible, continuing...");
             }
 
+        ExtentReportUtils.pass("User entered a valid State name in the 'State' field.");
 
         // Zip Code
             WaitUtils.waitForVisibility(shippingAddressPage.zipCodeShipping, 1);
@@ -81,15 +139,21 @@ public class US_13_Goychak {
             } else {
                 System.out.println("Zip Code field not visible, continuing...");
             }
+
+        ExtentReportUtils.pass("User entered a valid postcode code in the 'ZIP code' field.");
+
         ActionsUtils.actionsHoverOverOnElement(shippingAddressPage.saveAddressButton);
 
         shippingAddressPage.saveAddressButton.click();
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User clicked on the 'Save Address' button.");
+
         assertEquals("Address changed successfully.", shippingAddressPage.addressSuccessfullyChangedAlert.getText());
         WaitUtils.waitFor(3);
 
-
+        ExtentReportUtils.passAndCaptureScreenshot("User observes the success message.");
+        ExtentReportUtils.info("Validation works as expected: The shipping address is successfully added when all required fields are completed.");
     }
 
     @Test
@@ -97,17 +161,27 @@ public class US_13_Goychak {
         shippingAddressPage.firstNameShipping.clear();
         shippingAddressPage.firstNameShipping.sendKeys(faker.name().firstName());
 
+        ExtentReportUtils.pass("User entered a valid name in the 'First Name' field.");
+
         // Omitting Last Name
         shippingAddressPage.lastNameShipping.clear();
+
+        ExtentReportUtils.info("User left the 'Last name' field empty.");
 
         BrowserUtils.dropdownSelectByVisibleText(shippingAddressPage.countryDropDownShipping, "Antarctica");
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User selected 'Antarctica' from the Country dropdown.");
+
         // Omitting Street Address
         shippingAddressPage.streetAddressShipping.clear();
 
+        ExtentReportUtils.info("User left the 'Street address' field empty.");
+
         // Omitting City
         shippingAddressPage.cityShipping.clear();
+
+        ExtentReportUtils.info("User left the 'City' field empty.");
 
         // State Field
         WaitUtils.waitForVisibility(shippingAddressPage.stateShipping, 1);
@@ -119,22 +193,32 @@ public class US_13_Goychak {
             System.out.println("State field not visible, continuing...");
         }
 
+        ExtentReportUtils.pass("User entered a valid State name in the 'State' field.");
+
         // Omitting Zip Code
         shippingAddressPage.zipCodeShipping.clear();
+
+        ExtentReportUtils.info("User left the 'ZIP code' field empty.");
 
         ActionsUtils.actionsHoverOverOnElement(shippingAddressPage.saveAddressButton);
         shippingAddressPage.saveAddressButton.click();
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User clicked on the 'Save Address' button.");
 
         assertEquals(shippingAddressPage.incorrectLastName.getText(), "Last name is a required field.");
         assertEquals(shippingAddressPage.incorrectStreetAddress.getText(), "Street address is a required field.");
         assertEquals(shippingAddressPage.incorrectCity.getText(), "Town / City is a required field.");
         assertEquals(shippingAddressPage.incorrectZip.getText(), "Postcode / ZIP is a required field.");
 
+        ExtentReportUtils.passAndCaptureScreenshot("User observes multiple warning messages.");
+        ExtentReportUtils.info("Validation works as expected: Validation works as expected: Updating the shipping address with empty required fields is unsuccessful.");
+
     }
 
     @AfterMethod
     public void tearDown() {
+        ExtentReportUtils.flush();
         Driver.closeDriver();
     }
 }
--- src/test/java/myapp/tests/US_14_Goychak.java
+++ src/test/java/myapp/tests/US_14_Goychak.java
@@ -5,7 +5,6 @@ import myapp.pages.HomePage;
 import myapp.pages.StoreManager_Page;
 import myapp.pages.accountPage.Login_Page;
 import myapp.pages.accountPage.MyAccount_Page;
-import myapp.pages.accountPage.ShippingAddressPage;
 import myapp.utilities.*;
 
 import org.testng.annotations.AfterMethod;
@@ -20,6 +19,35 @@ import java.awt.event.KeyEvent;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+/*
+TC_01
+1- Navigate to https://allovercommerce.com/
+2- Click on the Sign In tab located in the top-right corner of the website.
+3- Enter valid vendor credentials (Email & Password) in the login fields.
+4- Click on the Sign-In button to log into the account.
+5- Scroll to the footer of the page and click on My Account under the My Account section.
+6- Click on the Store Manager tab on the left-hand side of the website.
+7- Hover on the Products tab on the left-hand side of the website.
+8- Click on the Add New tab on the right.
+9- Enter valid details in all required fields: (Product Title, Short Description and Full Description, Categories, Product Brands, Images...)
+10- Click on the Submit button in the bottom-right corner of the page.
+11- Assert that the confirmation message "Product successfully published."
+
+TC_02
+1- Navigate to https://allovercommerce.com/
+2- Click on the Sign In tab located in the top-right corner of the website.
+3- Enter valid vendor credentials (Email & Password) in the login fields.
+4- Click on the Sign-In button to log into the account.
+5- Scroll to the footer of the page and click on My Account under the My Account section.
+6- Click on the Store Manager tab on the left-hand side of the website.
+7- Hovers on the Products tab on the left-hand side of the website.
+8- Click on the Add New tab on the right.
+9- Enter valid details in some fields and leave one or more required fields empty, such as: Product Title, Short Description and Full Description, Categories, Product Brands, Images
+10- Click on the Submit button in the bottom-right corner of the page.
+11- Assert that the system displays an error message, such as: "Gallery Images: This field is required; Please insert Product Title before submit; Product cat: This field is required."
+
+*/
+
 public class US_14_Goychak {
     HomePage homePage;
     Login_Page loginPage;
@@ -28,13 +56,20 @@ public class US_14_Goychak {
     AddProduct_Page addProductPage;
 
     Robot robot;
-    String pathOfPicture1 = "C:\\Users\\KM\\Desktop\\shirts\\redshirt.png";
-    String pathOfPicture2 = "C:\\Users\\KM\\Desktop\\shirts\\shirt.png";
+    String pathOfImage1 = "C:\\Users\\KM\\Desktop\\shirts\\redshirt.png";
+    String pathOfImage2 = "C:\\Users\\KM\\Desktop\\shirts\\shirt.png";
+    //String imagePath1 = System.getProperty("user.dir") + "/src/test/resources/redshirt.png";
+    //String imagePath2 = System.getProperty("user.dir") + "/src/test/resources/shirt.png";
 
     @BeforeMethod
     public void setUp() throws AWTException {
 
+        ExtentReportUtils.createTestReport("AllOverCommerce Project Test Report for US_14", "Testing the Adding New Product functionality");
+
         Driver.getDriver().get(ConfigReader.getProperty("allOverCommerce_url"));
+
+        ExtentReportUtils.pass("User navigated to the page.");
+
         homePage = new HomePage();
         loginPage = new Login_Page();
         myAccountPage = new MyAccount_Page();
@@ -43,20 +78,31 @@ public class US_14_Goychak {
         robot = new Robot();
 
         homePage.signInOption.click();
+
+        ExtentReportUtils.pass("User clicked on Sign-In button.");
+
         loginPage.usernameEmailInput.sendKeys(ConfigReader.getProperty("allOverCommerce_vendorEmail"));
         loginPage.passwordInput.sendKeys(ConfigReader.getProperty("Password"));
         loginPage.signInButton.click();
 
+        ExtentReportUtils.pass("User entered valid credentials as a vendor.");
+
         ActionsUtils.actionsScrollDown();
         WaitUtils.waitFor(3);
         JSUtils.JSclickWithTimeout(homePage.myAccountTab);
+
+        ExtentReportUtils.pass("User scrolled down to the footer of the page and clicked on My Account tab.");
+
         myAccountPage.storeManagerMenuTab.click();
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User clicked on Store Manager tab.");
 
         ActionsUtils.actionsHoverOverOnElement(storeManagerPage.productsMenuTab);
         storeManagerPage.addNew.click();
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User hovered over the Products tab and clicked on Add New tab.");
 
     }
 
@@ -64,14 +110,20 @@ public class US_14_Goychak {
     public void test01() throws AWTException {
 
         BrowserUtils.dropdownSelectByIndex(addProductPage.productTypeDropdown, 0);
+
+        ExtentReportUtils.pass("User selected Simple Product option.");
+
         //Adding image
         addProductPage.addImage1.click();
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User clicked on Add Image box.");
+
         addProductPage.selectImage1.click();
         WaitUtils.waitFor(3);
 
         robot.delay(2000);
-        StringSelection selection = new StringSelection(pathOfPicture1);
+        StringSelection selection = new StringSelection(pathOfImage1);
         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
         robot.keyPress(KeyEvent.VK_CONTROL);
         robot.keyPress(KeyEvent.VK_V);
@@ -83,15 +135,20 @@ public class US_14_Goychak {
         addProductPage.selectPastedImage.click();
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User added first image to the gallery.");
+
         //Adding image to the gallery
         ActionsUtils.actionsHoverOverOnElement(addProductPage.addImage2);
         addProductPage.addImage2.click();
+
+        ExtentReportUtils.pass("User clicked on a second Add Image box.");
+
         ActionsUtils.actionsHoverOverOnElement(addProductPage.uploadImageTab);
         WaitUtils.waitFor(3);
         addProductPage.uploadImageTab.click();
         addProductPage.selectImage2.click();
         WaitUtils.waitFor(2);
-        StringSelection selection2 = new StringSelection(pathOfPicture2);
+        StringSelection selection2 = new StringSelection(pathOfImage2);
         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection2, null);
         robot.keyPress(KeyEvent.VK_CONTROL);
         robot.keyPress(KeyEvent.VK_V);
@@ -104,6 +161,8 @@ public class US_14_Goychak {
         addProductPage.addToGallery.click();
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User added second image to the gallery.");
+
         ActionsUtils.actionsHoverOverOnElement(addProductPage.virtualCheckbox);
         if (!addProductPage.virtualCheckbox.isSelected()) {
             addProductPage.virtualCheckbox.click();
@@ -115,15 +174,31 @@ public class US_14_Goychak {
             System.out.println("Downloadable Checkbox is not selected");
         }
 
+        ExtentReportUtils.pass("User checked the Virtual checkbox.");
+
         addProductPage.productTitle.sendKeys("T-shirt");
+
+        ExtentReportUtils.pass("User entered 'T-shirt' in the Product title field.");
+
         addProductPage.regularPrice.sendKeys("10");
+
+        ExtentReportUtils.pass("User entered '10' in the Regular price field.");
+
         addProductPage.salePrice.sendKeys("5");
         BrowserUtils.frameSwitchTo("//iframe[@id='excerpt_ifr']");
+
+        ExtentReportUtils.pass("User entered '5' in the Sale price field.");
+
         addProductPage.shortDescription.sendKeys("Red T-shirt");
+
+        ExtentReportUtils.pass("User entered 'Red T-shirt' to the short description text box.");
+
         Driver.getDriver().switchTo().parentFrame();
         BrowserUtils.frameSwitchTo("//iframe[@id='description_ifr']");
         addProductPage.description.sendKeys("Defacto Red T-shirt for Men");
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User entered 'Defacto Red T-shirt for Men' to the description text box.");
 
         Driver.getDriver().switchTo().parentFrame();
         if (!addProductPage.clothesCategory.isSelected()) {
@@ -131,11 +206,15 @@ public class US_14_Goychak {
         }
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User checked the 'Clothing' checkbox in the Category field.");
+
         ActionsUtils.actionsHoverOverOnElement(addProductPage.defactoProductBrand);
         if (!addProductPage.defactoProductBrand.isSelected()) {
             addProductPage.defactoProductBrand.click();
         }
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User checked the 'Defacto' checkbox in the Product brands field.");
 
         ActionsUtils.actionsHoverOverOnElement(addProductPage.manageStockCheckbox);
         if (!addProductPage.manageStockCheckbox.isSelected()) {
@@ -145,14 +224,22 @@ public class US_14_Goychak {
         }
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User checked the Manage Stock checkbox.");
+
         addProductPage.stockQuantity.clear();
         addProductPage.stockQuantity.sendKeys("1");
+
+        ExtentReportUtils.pass("User entered '1' in the Stock Quantity field.");
 
         ActionsUtils.actionsHoverOverOnElement(addProductPage.submitButton);
         addProductPage.submitButton.click();
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User clicked on Submit Button.");
+
         assertEquals(addProductPage.publishedTag.getText(), "Published");
+        ExtentReportUtils.passAndCaptureScreenshot("User observes the success message and 'Published' field appears at the top.");
+        ExtentReportUtils.info("Validation works as expected: A product is successfully added when all required fields are filled.");
 
     }
 
@@ -161,14 +248,19 @@ public class US_14_Goychak {
 
         BrowserUtils.dropdownSelectByIndex(addProductPage.productTypeDropdown, 0);
 
+        ExtentReportUtils.pass("User selected Simple Product option.");
+
         //Adding image
         addProductPage.addImage1.click();
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User clicked on Add Image box.");
+
         addProductPage.selectImage1.click();
         WaitUtils.waitFor(3);
 
         robot.delay(2000);
-        StringSelection selection = new StringSelection(pathOfPicture1);
+        StringSelection selection = new StringSelection(pathOfImage1);
         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
         robot.keyPress(KeyEvent.VK_CONTROL);
         robot.keyPress(KeyEvent.VK_V);
@@ -180,7 +272,15 @@ public class US_14_Goychak {
         addProductPage.selectPastedImage.click();
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User added first image to the gallery.");
+
+///////////////////////// Product Title is missing
+
+        ExtentReportUtils.info("User left the Product Title field empty.");
+
 ///////////////////////// Gallery image is missing
+
+        ExtentReportUtils.info("User did not add the second image to the gallery.");
 
         ActionsUtils.actionsHoverOverOnElement(addProductPage.virtualCheckbox);
         if (!addProductPage.virtualCheckbox.isSelected()) {
@@ -193,26 +293,39 @@ public class US_14_Goychak {
             System.out.println("Downloadable Checkbox is not selected");
         }
 
-///////////////////////// Product Title is missing
+        ExtentReportUtils.pass("User checked the Virtual checkbox.");
 
         addProductPage.regularPrice.sendKeys("10");
+
+        ExtentReportUtils.pass("User entered '10' in the Regular price field.");
+
         addProductPage.salePrice.sendKeys("5");
 
+        ExtentReportUtils.pass("User entered '5' in the Sale price field.");
+
 ///////////////////////// Short Description is missing
+
+        ExtentReportUtils.info("User left the Short description field empty.");
 
         BrowserUtils.frameSwitchTo("//iframe[@id='description_ifr']");
         addProductPage.description.sendKeys("Defacto Red T-shirt for Men");
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User entered 'Defacto Red T-shirt for Men' to the description text box.");
+
         Driver.getDriver().switchTo().parentFrame();
 
 ///////////////////////// Category is missing
+
+        ExtentReportUtils.info("User left the Category field unselected.");
 
         ActionsUtils.actionsHoverOverOnElement(addProductPage.defactoProductBrand);
         if (!addProductPage.defactoProductBrand.isSelected()) {
             addProductPage.defactoProductBrand.click();
         }
         WaitUtils.waitFor(3);
+
+        ExtentReportUtils.pass("User checked the 'Defacto' checkbox in the Product brands field.");
 
         ActionsUtils.actionsHoverOverOnElement(addProductPage.manageStockCheckbox);
         if (!addProductPage.manageStockCheckbox.isSelected()) {
@@ -222,26 +335,36 @@ public class US_14_Goychak {
         }
         WaitUtils.waitFor(3);
 
+        ExtentReportUtils.pass("User checked the Manage Stock checkbox.");
+
         addProductPage.stockQuantity.clear();
         addProductPage.stockQuantity.sendKeys("1");
+
+        ExtentReportUtils.pass("User entered '1' in the Stock Quantity field.");
 
         ActionsUtils.actionsHoverOverOnElement(addProductPage.submitButton);
         addProductPage.submitButton.click();
         WaitUtils.waitFor(3);
 
-        System.out.println("addProductPage.incorrectInput.getText() = " + addProductPage.incorrectInput.getText());
+        ExtentReportUtils.pass("User clicked on Submit Button.");
+
+        //System.out.println("addProductPage.incorrectInput.getText() = " + addProductPage.incorrectInput.getText());
 
         assertTrue(addProductPage.incorrectInput.getText().contains("Please insert Product Title before submit.\n" +
                 "Gallery Images: This field is required.\n" +
                 "Product cat: This field is required."));
 
-
+        ExtentReportUtils.passAndCaptureScreenshot("User observes multiple warning messages.");
+        ExtentReportUtils.info("Validation works as expected: Adding a product with empty required fields is unsuccessful.");
     }
 
 
     @AfterMethod
     public void tearDown() {
+
+        ExtentReportUtils.flush();
         Driver.closeDriver();
+
     }
 
 
